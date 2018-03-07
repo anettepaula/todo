c379812f38aaa4caec39af8c8cfcc8c988d951e5
ism06659681 <ism06659681@a04.informatica.escoladeltreball.org>
2018-03-07 12:12:24 +0100

    LDISD

--- todo02.py
+++ todo02.py
@@ -1,9 +1,13 @@
 #coding: utf-8
+
 meses = ("Error","enero","febrero","marzo","abril","marzo","junio","julio","agosto","septiembre","octubre","noviembre","diciembre")
 
 num = int(input("Dime un número: "))
 if num == 0:
-	print "Error."
+    print "Error."
+
+while num>12 :
+	num = int(input("Error. Dime otro número: "))
 
 else :
-	print meses[num]
+	print meses[num]	
--- todo06.py
+++ todo06.py
@@ -1,2 +1,19 @@
 #coding: utf-8
 listaPalabras = []
+palabras = []
+num = int(input("Dime cuantas palabras tiene la lista: "))
+for i in range(num):
+	pal = raw_input("Dime la palabra: ")
+	listaPalabras += [pal]
+
+prefijo = raw_input("Dime un prefijo: ")
+def inicianCon(listaPalabras, prefijo):
+	for i in(listaPalabras):
+		if (i.find(prefijo,0,len(prefijo))>=0):		
+			palabras.append(i)
+inicianCon(listaPalabras, prefijo)
+	
+print "Las palabras que comienzan con",prefijo,"son:",palabras
+
+print (inicianCon(listaPalabras, prefijo))
+	
